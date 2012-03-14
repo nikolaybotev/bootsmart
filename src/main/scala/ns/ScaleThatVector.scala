98cf990889af1826ea3b14da3da7a180ca56f139
Nikolay Botev <bono8106@yahoo.com>
2012-03-13 22:54:20 -0700

    xml version

--- src/main/scala/ns/ScaleThatVector.scala
+++ src/main/scala/ns/ScaleThatVector.scala
@@ -10,7 +10,7 @@ object ScaleThatVector extends App {
 
   case class Component(container: String, level: Int, name: String)
   case class Event(c: Component, start: Long, end: Long)
-  
+
   val inputFile = if (args.size > 0) args(0) else "nsboot"
   println("Reading file " + inputFile)
 
@@ -30,83 +30,76 @@ object ScaleThatVector extends App {
       .filter (!_.c.name.matches("Level[1-4]"))
       .filter (e => e.end - e.start >= msPerPixel)
       .sortBy (_.start)
-  
+
   val startTime = events map (_ start) reduce (_ min _)
   val endTime = events map (_ end) reduce (_ max _)
   val timeSpan = ((endTime - startTime) / 5000d).ceil.toInt * 5000
-  
+
   val lineHeight = 16
-  
+
   val pixelWidth = (timeSpan * pixelsPerMs).ceil.toLong + 300
   val pixelHeight = lineHeight * events.size + 30
-  
-  def time2px(t: Long) = ((t - startTime) * pixelsPerMs).round
-  
-  printf("netsmart start began %tT%nnetsmart start ended %tT%n", startTime, endTime)
-  
-  val file = new PrintWriter(inputFile + ".svg", "UTF-8")
-  
-  file.print("""<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
 
-<svg width="%dpx" height="%dpx" xmlns="http://www.w3.org/2000/svg">
-      
-  <rect x="0" y="0" width="100%%" height="100%%" style="fill: white;"/>
-  <g transform="translate(0, 20)" style="fill: none;">
-      """.format(pixelWidth, pixelHeight))
-      
+  def time2px(t: Long) = ((t - startTime) * pixelsPerMs).round
+
+  printf("netsmart start began %tT%nnetsmart start ended %tT%n", startTime, endTime)
+
   // Draw time axis labels
-  for (i <- 5000 to (timeSpan, 5000)) {
-    file.print("""
-        <text style="font-family: sans-serif; font-size: 9pt; fill: #000000; opacity: 0.7;" x="%d" y="0" dx="-0.5em" dy="-3">%ds</text>
-        """.format((i * pixelsPerMs).toInt, i/1000))
+  val axisLabels = for (i <- 5000 to (timeSpan, 5000)) yield {
+    <text style="font-family: sans-serif; font-size: 9pt; fill: #000000; opacity: 0.7;" x={(i * pixelsPerMs).toInt.toString} y="0" dx="-0.5em" dy="-3">{(i/1000).toString}s</text>
   }
-  
+
   // Draw vertical axis lines
-  file.print("""
-      <rect style="stroke: #e0e0e0; padding: 10px;" x="0" y="0" width="100%" height="100%"/>
-      <g style="stroke: #e0e0e0; stroke-width: 1px;" transform="translate(0.5, 0.5)">
-      """)
-  for (i <- 0 to (timeSpan, 1000)) {
-    file.print("""
-        <line%2$s x1="%1$d" y1="0" x2="%1$d" y2="%3$d"/>
-        """.format((i * pixelsPerMs).toInt, if (i % 5000 == 0) " style=\"stroke: #d0d0d0;\"" else "", events.size * lineHeight))
+  val bottom = events.size * lineHeight
+  val axisLinesVert = for (i <- 0 to (timeSpan, 1000)) yield {
+    val x = (i * pixelsPerMs).toInt.toString
+    <line style={if (i % 5000 == 0) "stroke: #d0d0d0;" else ""} x1={x} y1="0" x2={x} y2={bottom.toString}/>
   }
-  
+
   // Draw horizontal axis lines
-  for (i <- events.size to events.size) {
-    file.print("""
-        <line x1="0" y1="%1$d" x2="%2$d" y2="%1$d"/>
-        """.format(i * lineHeight, pixelWidth))
+  val axisLinesHoriz = for (i <- events.size to events.size) yield {
+    val y = i * lineHeight
+    <line x1="0" y1={y.toString} x2={pixelWidth.toString} y2={y.toString}/>
   }
-  file.print("""
-      </g>
-      """)
 
   // Draw process
   var y = 0
   var maxEnd = startTime
-  for (event <- events) {
+  val eventNodes = events flatMap { event =>
     val label = event.c.level + " " + event.c.container + " / " + event.c.name
     val x1 = time2px(event.start)
     val x2 = time2px(event.end)
     val gapped = (event.start - maxEnd) >= msPerPixel * 3
-    val gapLabel = if (gapped) "(%.1f s gap) ".format((event.start - maxEnd)/1000d) else "" 
-    file.print("""
-        <rect x="%d" y="%d" width="%d" height="%d" transform="translate(0.5, 0.5)" style="stroke: #a0a0a0; fill: #%s;"/>
-        """.format(x1, y, x2 - x1, lineHeight, if (gapped) "ff6600" else "e0e0e0"))
-    file.print("""
-        <text transform="translate(%d,%d)" style="font-family: sans-serif; font-size: 9pt; fill: #%s; opacity: 0.7; font-weight: %s;" dx="4" dy="12" x="%d" y="0">%s</text>            
-        """.format(x1, y, if (gapped) "ff0000" else "000000", if (gapped) "bold" else "normal", x2 - x1, gapLabel + label))
+    val gapLabel = if (gapped) "(%.1f s gap) ".format((event.start - maxEnd)/1000d) else ""
+    val nodes =
+        <rect x={ x1.toString } y={ y.toString } width={(x2 - x1).toString} height={lineHeight.toString} transform="translate(0.5, 0.5)" style={ "stroke: #a0a0a0; fill: #%s;".format(if (gapped) "ff6600" else "e0e0e0") }/> ++
+        <text transform={ "translate(%d,%d)".format(x1, y) }
+          style={ "font-family: sans-serif; font-size: 9pt; fill: #%s; opacity: 0.7; font-weight: %s;".format(if (gapped) "ff0000" else "000000", if (gapped) "bold" else "normal") }
+          dx="4" dy="12" x={ (x2 - x1).toString } y="0">{ gapLabel + label }</text>
     y += lineHeight
     maxEnd = maxEnd max event.end
+    nodes
   }
-      
-  file.print("""
-  </g>
-</svg>      
-      """)
-      
-  file.close
-  
+
+  val svg =
+  <svg width={ pixelWidth.toString + "px" } height={ pixelHeight.toString } xmlns="http://www.w3.org/2000/svg">
+    <rect x="0" y="0" width="100%" height="100%" style="fill: white;"/>
+    <g transform="translate(0, 20)" style="fill: none;">
+      { axisLabels }
+        <rect style="stroke: #e0e0e0; padding: 10px;" x="0" y="0" width="100%" height="100%"/>
+        <g style="stroke: #e0e0e0; stroke-width: 1px;" transform="translate(0.5, 0.5)">
+          { axisLinesVert }
+          { axisLinesHoriz }
+        </g>
+      { eventNodes }
+    </g>
+  </svg>
+
+  val file = new PrintWriter(inputFile + ".svg", "UTF-8")
+  try {
+    file.append(svg.toString)
+  } finally {
+    file.close
+  }
+
 }
